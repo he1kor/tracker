34e336ae509a67d3da633197e4cc90dde4bd32b3
he1kor <helkor.rightway@gmail.com>
2022-05-28 03:51:25 +0300

    Protected API key

--- app/src/main/java/com/helkor/project/global/YandexMapkit.java
+++ app/src/main/java/com/helkor/project/global/YandexMapkit.java
@@ -2,6 +2,7 @@ package com.helkor.project.global;
 
 import android.app.Activity;
 
+import com.helkor.project.BuildConfig;
 import com.helkor.project.R;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
@@ -9,7 +10,7 @@ import com.yandex.mapkit.MapKitFactory;
 public class YandexMapkit {
 
     public static MapKit initAPI(Activity activity) {
-        String MAPKIT_KEY = activity.getString(R.string.key);
+        String MAPKIT_KEY = BuildConfig.API_KEY;
         MapKitFactory.setApiKey(MAPKIT_KEY);
         MapKitFactory.initialize(activity);
         return MapKitFactory.getInstance();
